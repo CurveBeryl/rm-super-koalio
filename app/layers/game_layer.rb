09fac4d834cccac103a25ecad49c538a5891cb77
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-09-07 17:24:52 -0700

    Step 2. Add Blue Sky Background

--- app/layers/game_layer.rb
+++ app/layers/game_layer.rb
@@ -2,6 +2,9 @@ class GameLayer < Joybox::Core::Layer
   include Joybox::TMX
   
   def on_enter
+    @blue_sky = CCLayerColor.alloc.initWithColor([100, 100, 250, 255])
+    self << @blue_sky
+    
     @tile_map = TileMap.new file_name: 'level1.tmx'
     self << @tile_map
   end
